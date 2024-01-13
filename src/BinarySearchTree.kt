9912a9dff5df4c87c04501460ae74549d5ba59a2
Morvin-Ian <morvinian@gmail.com>
2024-01-13 23:46:57 +0300

    finished setting the BST

--- src/BinarySearchTree.kt
+++ src/BinarySearchTree.kt
@@ -1,24 +1,45 @@
-interface BSTBluePrint<T:Any> {
-    fun add(value:T)
-    fun isPresent(value:T):T?
+open class BinaryNode<T:Comparable<T>>(val data:T) {
+    var left:BinaryNode<T>? = null
+    var right:BinaryNode<T>? = null
 }
 
-class BST<T:Any>:BSTBluePrint<T>{
-    var root:Node<T>? = null
-    var left: Node<T>? = null
-    var right: Node<T>? = null
-    override fun add(value: T) {
-        val newNode = Node(value)
+class BST<T:Comparable<T>>{
+    var root: BinaryNode<T>? = null
+     fun add(value: T) {
+        val newNode = BinaryNode<T>(value)
 
         if (root == null) {
             root = newNode
             return
         }
-        // CONTINUE
-    }
 
-    override fun isPresent(value: T): T? {
+         var current: BinaryNode<T> = root ?: return
+
+         while (true) {
+             if(current.data == value){
+                 return
+             }else if(value < current.data){
+                 if (current.left == null) {
+                     current.left = newNode
+                     return
+                 }
+
+                 current.left?.let { current = it }
+             }else if( value > current.data){
+                 if (current.right == null) {
+                     current.right = newNode
+                     return
+                 }
+
+                 current.right?.let { current = it }
+             }
+         }
+     }
+
+     fun isPresent(value: Int): Int? {
         TODO("Not yet implemented")
     }
 
 }
+
+
--- src/Main.kt
+++ src/Main.kt
@@ -3,5 +3,9 @@ fun main() {
 
     val binary = BST<Int>()
     binary.add(3)
-    println(binary.root)
+    binary.add(30)
+    binary.add(13)
+    binary.add(19)
+    binary.add(2)
+    println()
 }
